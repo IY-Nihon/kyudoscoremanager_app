e04a79e2df1a983c495ab5b463abaaf1eac93df1
Backup <backup@example.com>
2026-05-11 08:17:29 +0900

    Security: APIキーを含む IS_WEB_199.js を .gitignore に追加し、今後のコミットから除外

--- src/IS_WEB_199.js
+++ src/IS_WEB_199.js
@@ -11,4 +11,16 @@ const m = module;
 const _e = exports;
 const d = (typeof dependencyMap !== 'undefined' ? dependencyMap : []);
 
-"use strict";Object.defineProperty(_e,'__esModule',{value:!0}),Object.defineProperty(_e,"IS_WEB",{enumerable:!0,get:function(){return e}}),Object.defineProperty(_e,"IS_IOS",{enumerable:!0,get:function(){return t}}),Object.defineProperty(_e,"WEB_TOP_PADDING",{enumerable:!0,get:function(){return n}}),Object.defineProperty(_e,"SAFE_TOP_PADDING",{enumerable:!0,get:function(){return u}}),Object.defineProperty(_e,"GEMINI_API_KEY",{enumerable:!0,get:function(){return c}}),require("./module_98");const e=(()=>{try{return!0}catch(e){return'undefined'!=typeof window}})(),t=!e&&!1,n=60,u=e?n:0,c="AIzaSyDyeWxsH19qD2Xyqlb4rRPd1IzmADQZnkI"
+"use strict";
+Object.defineProperty(_e, '__esModule', { value: !0 });
+Object.defineProperty(_e, "IS_WEB", { enumerable: !0, get: function () { return e } });
+Object.defineProperty(_e, "IS_IOS", { enumerable: !0, get: function () { return t } });
+Object.defineProperty(_e, "WEB_TOP_PADDING", { enumerable: !0, get: function () { return n } });
+Object.defineProperty(_e, "SAFE_TOP_PADDING", { enumerable: !0, get: function () { return u } });
+Object.defineProperty(_e, "GEMINI_API_KEY", { enumerable: !0, get: function () { return c } });
+require("./module_98");
+const e = (() => { try { return !0 } catch (e) { return 'undefined' != typeof window } })();
+const t = !e && !1;
+const n = 60;
+const u = e ? n : 0;
+const c = "AQ.Ab8RN6JqTILCUJiAAoO6GA5DruUq_EJw3PkX4n2lHwSLcCURmQ";
--- src/module_592.js
+++ src/module_592.js
@@ -11,4 +11,43 @@ const m = module;
 const e = exports;
 const d = (typeof dependencyMap !== 'undefined' ? dependencyMap : []);
 
-"use strict";Object.defineProperty(e,'__esModule',{value:!0}),Object.defineProperty(e,"getShadowStyle",{enumerable:!0,get:function(){return t}});require("./module_98");const t=t=>{const n='number'==typeof t?{elevation:t}:t,{shadowColor:s="#000000",shadowOffset:o={width:0,height:Math.floor((n.elevation||4)/2)},shadowOpacity:u=.2,shadowRadius:h=n.elevation||4,elevation:l=n.elevation||4}=n;if(typeof window!=='undefined'){const t=s.startsWith('#')?r(s,u):s;return{boxShadow:`${o.width}px ${o.height}px ${h}px ${t}`}}return{shadowColor:s,shadowOffset:o,shadowOpacity:u,shadowRadius:h,elevation:l}},r=(t,r)=>{if(!t)return`rgba(0,0,0,${r})`;if(t.startsWith('rgba'))return t;if('black'===t)return`rgba(0,0,0,${r})`;if('white'===t)return`rgba(255,255,255,${r})`;let n=0,s=0,o=0,u=t.replace('#','');if(3===u.length)n=parseInt(u[0]+u[0],16),s=parseInt(u[1]+u[1],16),o=parseInt(u[2]+u[2],16);else{if(6!==u.length)return t;n=parseInt(u.substring(0,2),16),s=parseInt(u.substring(2,4),16),o=parseInt(u.substring(4,6),16)}return`rgba(${n}, ${s}, ${o}, ${r})`}
+"use strict";
+Object.defineProperty(e, '__esModule', { value: !0 });
+Object.defineProperty(e, "getShadowStyle", { enumerable: !0, get: function () { return getShadowStyle } });
+require("./module_98");
+
+const getShadowStyle = (config) => {
+    const n = 'number' == typeof config ? { elevation: config } : config;
+    const {
+        shadowColor: s = "#000000",
+        shadowOffset: o = { width: 0, height: Math.floor((n.elevation || 4) / 2) },
+        shadowOpacity: u = .2,
+        shadowRadius: h = n.elevation || 4,
+        elevation: l = n.elevation || 4
+    } = n;
+
+    if (typeof window !== 'undefined') {
+        const t = s.startsWith('#') ? hexToRgba(s, u) : s;
+        return { boxShadow: `${o.width}px ${o.height}px ${h}px ${t}` };
+    }
+    return { shadowColor: s, shadowOffset: o, shadowOpacity: u, shadowRadius: h, elevation: l };
+};
+
+const hexToRgba = (color, opacity) => {
+    if (!color) return `rgba(0,0,0,${opacity})`;
+    if (color.startsWith('rgba')) return color;
+    if ('black' === color) return `rgba(0,0,0,${opacity})`;
+    if ('white' === color) return `rgba(255,255,255,${opacity})`;
+    let n = 0, s = 0, o = 0, u = color.replace('#', '');
+    if (3 === u.length) {
+        n = parseInt(u[0] + u[0], 16);
+        s = parseInt(u[1] + u[1], 16);
+        o = parseInt(u[2] + u[2], 16);
+    } else {
+        if (6 !== u.length) return color;
+        n = parseInt(u.substring(0, 2), 16);
+        s = parseInt(u.substring(2, 4), 16);
+        o = parseInt(u.substring(4, 6), 16);
+    }
+    return `rgba(${n}, ${s}, ${o}, ${opacity})`;
+};
